56388a351c080ff8d8a98fc09d2b382cd6d984eb
ch-r <chandni.rajendran@gmail.com>
2017-02-11 04:12:26 +0530

    update

--- sketch.js
+++ sketch.js
@@ -1,9 +1,13 @@
-PImage img;
+ar img;  // Declare variable 'img'.
 
-void setup() {
-  img = loadImage("test.jpg");
+function setup() {
+  createCanvas(720, 400);
+  img = loadImage("test.jpg");  // Load the image
 }
 
-void draw() {
+function draw() {
+  // Displays the image at its actual size at point (0,0)
   image(img, 0, 0);
+  // Displays the image at point (0, height/2) at half size
+  image(img, 0, height/2, img.width/2, img.height/2);
 }
